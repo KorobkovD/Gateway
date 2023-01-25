9eb5f2feb3d10e55f52a444c06320eec214e0557
KorobkovD <den-kor.int@mail.ru>
2023-01-25 12:53:52 +0300

    add logging

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,8 +1,35 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Host.UseSerilog((_, configuration) => configuration.WriteTo.Console()
+                                                               .Enrich.FromLogContext()
+                                                               .Enrich.WithThreadId()
+                                                               .Enrich.WithProcessId()
+                                                               .Enrich.WithHttpRequestId());
+}
+else
+{
+    var logFilePath = Path.Combine(AppContext.BaseDirectory, "logs", "gateway_.log");
+    builder.Host.UseSerilog((_, configuration) =>
+                                configuration.WriteTo.Console()
+                                             .WriteTo.File(logFilePath, retainedFileCountLimit: 7,
+                                                           rollingInterval: RollingInterval.Day,
+                                                           restrictedToMinimumLevel: LogEventLevel.Information,
+                                                           outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff z} [{Level:u3}] {IPAddress} [{RequestId}] {SourceContext}: {Message:lj}{NewLine}{Exception}")
+                                             .Enrich.FromLogContext()
+                                             .Enrich.WithThreadId()
+                                             .Enrich.WithProcessId()
+                                             .Enrich.WithHttpRequestId());
+}
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
@@ -10,4 +37,5 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseOcelot().Wait();
 app.MapControllers();
+app.UseSerilogRequestLogging();
 app.Run();
